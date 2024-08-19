d44d64fbdb91d94504ebde8a131ab376ac4a9c11
Anastasiya Bessmertnova <avbessmertnova@gmail.com>
2024-08-20 00:36:56 +0400

    HW13

--- src/test/java/TestBaseRemote.java
+++ src/test/java/TestBaseRemote.java
@@ -16,7 +16,7 @@ public class TestBaseRemote {
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
-        Configuration.remote = System.getProperty("remote", "selenoid.autotests.cloud");
+        Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
 
